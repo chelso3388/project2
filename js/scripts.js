6ccb227c4a5bf3cd087368a480f88caf40413c6b
Chelsea L Gifford <chelsealgifford@Chelseas-MBP.lan>
2019-12-01 16:41:28 -0800

    add questions into my javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,39 @@
-$(document).ready(function() {
-  $("button#green").click(function() {
-    $("body").removeClass();
-    $("body").addClass("green-background");
-  });
+var questions = (
+  {
+    prompt: "What Color are Bananas?\n(a) Blue\n\(b) Purple\n(c) Yellow\n(d) Orange",
+    answer: "c"
+  },
+  {
+    prompt: "What state only borders one state?\n(a) New York\n\(b) Hawaii\n(c) Alaska\n(d) Maine",
+    answer: "d"
+  },
+  {
+    prompt: "What flavor is the green Haribo gummy bear?\n(a) Strawberry\n\(b) Lime\n(c) Green Apple\n(d) Cherry",
+    answer: "a"
+  },
 
-  $("button#yellow").click(function() {
-    $("body").removeClass();
-    $("body").addClass("yellow-background");
-  });
-
-  $("button#red").click(function() {
-    $("body").removeClass();
-    $("body").addClass("red-background");
-  });
-});
+  {
+    prompt: "What Programming Language is named after Coffee?\n(a) Ruby\n\(b) Java\n(c) JavaScript\n(d) Phython",
+    answer: "b"
+  },
+  {
+    prompt: "what nut is a member of the Peach family\n(a) Almond\n\(b) Peanuts\n(c) Walnuts\n(d) Pistachio",
+    answer: "a"
+  },
+  {
+    prompt: "What Programming Language is named after a British comedy group?\n(a) JavaScript\n\(b) Phython\n(c) Java\n(d) Ruby",
+    answer: "b"
+  },
+  {
+    prompt: "What state has the most NFL teams?\n(a) Florida\n\(b) New York\n(c) California\n(d) Oregon",
+    answer: "c"
+  },
+  {
+    prompt: "Where was Hawaiian pizza invented?\n(a) Hawaii\n\(b) Canada\n(c) England\n(d) Middle-Earth",
+    answer: "b"
+  },
+  {
+    prompt: "What programming language runs in all browsers and requires no installation?\n(a) JavaScript\n\(b) Ruby\n(c) Java\n(d) Phython",
+    answer: "a"
+  },
+)
